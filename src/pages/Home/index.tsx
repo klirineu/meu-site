592a2124895189e760154cb321e3d7992d230139
LorranSaldanha <lorranollyveiracode@gmail.com>
2022-12-09 14:09:28 -0300

    Removendo pasta Main

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,13 +1,10 @@
 import { Header } from '../../components/Header'
-// import { Main } from '../../components/Main'
-import { MainComponent } from '../../components/Main'
 import { Container } from './styles'
 
 export function Home() {
   return (
     <Container>
       <Header />
-      <MainComponent />
     </Container>
   )
 }
